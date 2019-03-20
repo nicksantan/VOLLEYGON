4fb45a23cdd71f5f017f67a099962b1c83785942
Nick Santaniello <nick@robysoft.net>
2019-03-19 18:55:48 -0700

    Added FPS Manager script and removed movie texture

--- Assets/PlayHowToMovieScript.cs
+++ Assets/PlayHowToMovieScript.cs
@@ -2,15 +2,15 @@
 using System.Collections;
 
 public class PlayHowToMovieScript : MonoBehaviour {
-	public MovieTexture movTexture;
+//	public MovieTexture movTexture;
 	public GameObject pressStartAnimation;
 	private bool fadedOut;
 
 	void Start () {
 		fadedOut = false;
 		//QualitySettings.vSyncCount = 0;
-		GetComponent<Renderer>().material.mainTexture = movTexture;
-		movTexture.Play();
+//		GetComponent<Renderer>().material.mainTexture = movTexture;
+///	movTexture.Play();
 	}
 
 	// Update is called once per frame

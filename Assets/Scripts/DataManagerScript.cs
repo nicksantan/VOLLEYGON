e7ea209201a905d99ae01e0e129d154713bf740f
Nick Santaniello <nick@robysoft.net>
2023-10-20 22:06:19 -0400

    Added robysoft and arcade commons logos

--- Assets/Scripts/DataManagerScript.cs
+++ Assets/Scripts/DataManagerScript.cs
@@ -123,7 +123,7 @@ public class DataManagerScript : MonoBehaviour {
 
         // Version number
         // TODO: Uncomment public property instead?
-		version = "V2.0.0";
+		version = "V2.0.1";
 
         // Determine if on Xbox
         xboxMode = Application.platform == RuntimePlatform.XboxOne;
